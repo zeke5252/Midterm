178cd115946906123aec9a381e84a31a64da54b8
Zeke Lin <zeke5252@yahoo.com.tw>
2020-01-17 17:31:56 +0800

    Remove unnecessary modules from react-router-dom

--- src/components/index.js
+++ src/components/index.js
@@ -1,50 +1,46 @@
-import React from "react";
-import { HashRouter, Route, Switch, Link, Router } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 class StartSec extends React.Component {
-    render() {
-        return (
-            <h1>{this.props.title}</h1>
-        )
-    }
+  render () {
+    return <h1>{this.props.title}</h1>
+  }
 }
 
 class ReactSec extends React.Component {
-    render(){
-        return (
-            <div>
-                <ul>
-                    <li>React Element</li>
-                    <li>React Component</li>
-                    <li>React Lifecycle</li>
-                </ul>
-                <Link to='/'>回到首頁</Link>
-            </div>
-        )
-    }
+  render () {
+    return (
+      <div>
+        <ul>
+          <li>React Element</li>
+          <li>React Component</li>
+          <li>React Lifecycle</li>
+        </ul>
+        <Link to='/'>回到首頁</Link>
+      </div>
+    )
+  }
 }
 
 class ReduxSec extends React.Component {
-    render(){
-        return (
-            <div>
-                <ul>
-                    <li>Redux 簡介</li>
-                    <li>Redux 核心操作</li>
-                    <li>React Redux</li>
-                </ul>
-                <Link to='/'>回到首頁</Link>
-            </div>
-        )
-    }
+  render () {
+    return (
+      <div>
+        <ul>
+          <li>Redux 簡介</li>
+          <li>Redux 核心操作</li>
+          <li>React Redux</li>
+        </ul>
+        <Link to='/'>回到首頁</Link>
+      </div>
+    )
+  }
 }
 
 class ErrorSec extends React.Component {
-    render(){
-        return (
-            <h2>這位朋友，您輸入的網址有誤喔！</h2>
-        )
-    }
+  render () {
+    return <h2>這位朋友，您輸入的網址有誤喔！</h2>
+  }
 }
 
-export  { StartSec, ReactSec, ReduxSec, ErrorSec }
+export { StartSec, ReactSec, ReduxSec, ErrorSec }
--- src/index.js
+++ src/index.js
@@ -1,54 +1,71 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { HashRouter, Route, Switch, Link, Router } from "react-router-dom";
-import { StartSec, ReactSec, ReduxSec, ErrorSec } from "./components";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HashRouter, Route, Switch, Link } from 'react-router-dom'
+import { StartSec, ReactSec, ReduxSec, ErrorSec } from './components'
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            apiData: {},
-            headline:'',
-            tempRoutes:{
-                reactRoute:'',
-                reduxRoute:''
-            }
-        }
+  constructor (props) {
+    super(props)
+    this.state = {
+      apiData: {},
+      headline: '',
+      tempRoutes: {
+        reactRoute: '',
+        reduxRoute: ''
+      }
     }
+  }
 
-    componentDidMount(){
-        fetch('https://cwpeng.github.io/live-records-samples/data/content.json')
-        .then(res => res.json())
-        .then(result => this.setState({
-            apiData: result
-        }))
-        .then(res => {
-            this.setState({
-            headline:this.state.apiData.headline,
-            tempRoutes:{
-                reactRoute:this.state.apiData.chapters[1].key,
-                reduxRoute:this.state.apiData.chapters[2].key
-            }
-        })}
-        )
-    }
+  componentDidMount () {
+    fetch('https://cwpeng.github.io/live-records-samples/data/content.json')
+      .then(res => res.json())
+      .then(result =>
+        this.setState({
+          apiData: result
+        })
+      )
+      .then(res => {
+        this.setState({
+          headline: this.state.apiData.headline,
+          tempRoutes: {
+            reactRoute: this.state.apiData.chapters[1].key,
+            reduxRoute: this.state.apiData.chapters[2].key
+          }
+        })
+      })
+  }
 
-    render() {
-        let routes = this.state
-        return (
-            <div>
-                <Link to='/'>快速開始</Link>
-                <Link to={'/'+ this.state.tempRoutes.reactRoute}>React 基礎</Link>
-                <Link to={'/'+ this.state.tempRoutes.reduxRoute}>Redux 基礎</Link>
-                <Switch>
-                    <Route path='/' component={() => <StartSec title={this.state.headline} />} exact />
-                    <Route path={'/'+ this.state.tempRoutes.reactRoute} component={ReactSec} />
-                    <Route path={'/'+ this.state.tempRoutes.reduxRoute} component={ReduxSec} />
-                    <Route component={ErrorSec} />
-                </Switch>
-            </div>
-        )
-    }
+  render () {
+    let routes = this.state
+    return (
+      <div>
+        <Link to='/'>快速開始</Link>
+        <Link to={'/' + this.state.tempRoutes.reactRoute}>React 基礎</Link>
+        <Link to={'/' + this.state.tempRoutes.reduxRoute}>Redux 基礎</Link>
+        <Switch>
+          <Route
+            path='/'
+            component={() => <StartSec title={this.state.headline} />}
+            exact
+          />
+          <Route
+            path={'/' + this.state.tempRoutes.reactRoute}
+            component={ReactSec}
+          />
+          <Route
+            path={'/' + this.state.tempRoutes.reduxRoute}
+            component={ReduxSec}
+          />
+          <Route component={ErrorSec} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
-ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'))
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById('root')
+)
